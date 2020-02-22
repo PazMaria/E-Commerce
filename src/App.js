3715b7bac70555a4c923c7c5073e520d5a289d1d
Maria Paz <pazguada@gmail.com>
2020-02-22 10:00:03 -0700

    Created components

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import ProductList from "./components/ProductList";
+import Default from "./components/Default";
+import Details from "./components/Details";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
